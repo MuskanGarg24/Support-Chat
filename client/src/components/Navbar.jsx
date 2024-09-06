4931b55e95dff8f92b2b4f312202a1a32e0ce782
muskan garg <muskangarg02270@gmail.com>
2024-09-06 16:23:01 +0530

    navbar

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,85 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Bars3Icon } from "@heroicons/react/24/outline";
+
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("branchInternational");
+    navigate("/login");
+  };
+
+  return (
+    <nav>
+      <div className="w-full px-7">
+        <div className="flex justify-between">
+          {/* Primary menu and logo */}
+          <div className="flex items-center gap-16 my-9">
+            {/* logo */}
+            <div>
+              <Link
+                href="/"
+                className="text-lg font-bold text-gray-700 items-center "
+              >
+                <span>Branch International</span>
+              </Link>
+            </div>
+            {/* primary */}
+            <div className="hidden lg:flex gap-8 ">
+              <Link href="/" className="font-medium hover:text-purple-500">
+                My Queries
+              </Link>
+              <Link href="#" className="font-medium hover:text-purple-500">
+                Resolved Queries
+              </Link>
+            </div>
+          </div>
+          {/* secondary */}
+          <div className="flex gap-6">
+            <div className="hidden lg:flex items-center gap-5">
+              <Link to="/signup">
+                <button className="bg-purple-600 px-3 py-2 text-white rounded-lg hover:bg-purple-500 font-medium">
+                  Signup
+                </button>
+              </Link>
+              <button
+                className="bg-purple-600 px-3 py-2 text-white rounded-lg hover:bg-purple-500 font-medium"
+                onClick={handleLogout}
+              >
+                Logout
+              </button>
+            </div>
+            {/* Mobile navigation toggle */}
+            <div className="lg:hidden flex items-center">
+              <button onClick={() => setToggleMenu(!toggleMenu)}>
+                <Bars3Icon className="h-6" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* mobile navigation */}
+      <div
+        className={`fixed z-40 w-full bg-gray-100 overflow-hidden flex flex-col lg:hidden gap-12  origin-top duration-700 ${
+          !toggleMenu ? "h-0" : "h-full"
+        }`}
+      >
+        <div className="px-8">
+          <div className="flex flex-col gap-8 font-bold tracking-wider">
+            <Link href="#" className="mt-5">
+              My Queries
+            </Link>
+            <Link href="#">Resolved Queries</Link>
+            <Link href="#">Signup</Link>
+            <Link href="#">Logout</Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
