dd87c493b8e22b737783236a4af97ca69f0f1df4
muskan garg <muskangarg02270@gmail.com>
2024-09-07 11:59:04 +0530

    deploy backend url

--- client/src/utils/routes.js
+++ client/src/utils/routes.js
@@ -1,5 +1,6 @@
 // Desc: Routes for the client side
-export const host = "http://localhost:5000";
+// export const host = "http://localhost:5000";
+export const host = "https://cms-web-app.onrender.com";
 export const signupRoute = `${host}/api/user/signup`;
 export const loginRoute = `${host}/api/user/login`;
 export const addQuery = `${host}/api/queries/add`;
